4cd6252063bf4d15ee24525be62e27673bec9241
parthdhake <parthdhake@gmail.com>
2021-09-04 16:59:30 +0530

    Footer Added

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p
+          style={{
+            bottom: 0,
+            textAlign: "center",
+            left: 0,
+            width: "100%",
+          }}
+        >
+          Copyright&copy; 2017 Piyush Ingale
+        </p>
+      </footer>
+    </div>
+  );
+}
